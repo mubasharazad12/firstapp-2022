55d63b6f37bc0df2ecfe46f3ad5e94b96ca5df4b
mubasharazad12 <mubasharazad3@gmail.com>
2022-06-30 18:39:03 +0500

    added movies api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import "./App.css"
+import SearchIcon from "./search.svg"
+import Movies  from "./Movies";
 // f96712af
 
 const API_URL = 'http://www.omdbapi.com/?apikey=f96712af';
 
+
+
 const App = () => {
+
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        console.log(data);
+        setMovies(data.Search);
+        console.log(data.Search);
     }
 
     useEffect(() => {
@@ -18,7 +25,45 @@ const App = () => {
     }, []);
 
     return(
-        <h1> App </h1>
+       <div className="app">
+            <h1>Movie Land</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies" 
+                    value="Batman"
+                    onChange={() => {}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="Search icon"
+                    onClick={() => {}}
+                />
+            </div>
+
+            {
+                (() => {
+                    if(movies.length > 0) {
+                        return (  
+                          <div className="container"> 
+                            {movies.map((movie)=> (
+                                <Movies movie={movie} />
+                            ) )}
+                          </div>
+                        ) 
+                      } else {
+                          return (
+                            <div className="empty"> 
+                                <h2>No Movies Found</h2>
+                             </div>
+                          )
+                      }
+                })
+            }
+
+       </div>
+
+           
     );
 
 }
