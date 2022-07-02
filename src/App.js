830a8a16c221ba53d719f65c0290c0c8cace90bf
mubasharazad12 <mubasharazad3@gmail.com>
2022-07-02 13:53:07 +0500

    search movies fuctionality

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const API_URL = 'http://www.omdbapi.com/?apikey=f96712af';
 const App = () => {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -21,7 +22,7 @@ const App = () => {
     }
 
     useEffect(() => {
-        searchMovies('Batman');
+        searchMovies('Movie');
     }, []);
 
     return(
@@ -31,13 +32,13 @@ const App = () => {
             <div className="search">
                 <input 
                     placeholder="Search for movies" 
-                    value="Batman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                     src={SearchIcon}
                     alt="Search icon"
-                    onClick={() => {}}
+                    onClick={() => searchMovies(searchTerm)}
                 />
             </div>
 
