037c35411c8031e659bb4d82f9d958ae533fc36e
mubasharazad12 <mubasharazad3@gmail.com>
2022-07-20 18:44:16 +0500

    berro task completed

--- src/Components/ToDoApp/Blog.jsx
+++ src/Components/ToDoApp/Blog.jsx
@@ -3,20 +3,52 @@ import useFetch from "../../Hooks/useFetch";
 import '../../Assets/css/todo.css'
 import Loading from "../Loading";
 import { Link } from "react-router-dom";
+import React from 'react';
+import { useState } from "react";
+
 
 const Blog = () => {
 
+ 
+
     const { data : blogs, loading, error } = useFetch("http://localhost:8000/users")
-   
+
+    const [count, setCount] = useState([]);
+
+
+
     return (
 
+        
+
         <div className="blog content">
+
+            <input
+                    type="button"
+                    value="Add clone"
+                    onClick={(e) =>setCount(count + 1)}
+                />
+
             <button className="add-button">
                 <Link to="/todoapp/create">   Add Blog </Link> 
             </button>
             { error && <div> {error} </div>}
             { loading &&  <Loading />}
             { blogs && <BlogList blogs={blogs} /> }
+
+
+                    
+
+       
+                    {Array.from(count).map((key, index) => (
+                                
+                                <div key={index}>
+
+                        { blogs && <BlogList blogs={blogs} /> }
+                                </div>
+                    ))}
+
+
 
         </div> 
         
