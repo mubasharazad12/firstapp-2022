87f34af3b754158fb51528570eaecb13047b5687
mubasharazad12 <mubasharazad3@gmail.com>
2022-07-20 18:44:34 +0500

    .

--- src/Components/ToDoApp/BlogList.jsx
+++ src/Components/ToDoApp/BlogList.jsx
@@ -1,12 +1,15 @@
+
 import { Link } from "react-router-dom";
 
 const BlogList = ({blogs}) => {
 
+  
+
     return(
         <div className="blog-list">
-
+            
             {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
+                <div className="blog-preview" id="duplicater" key={blog.id}>
                     <Link to={`/todoapp/blogdetails/${blog.id}`}>
                         <h2>{blog.name}</h2>
                         <p>{blog.work}</p>
@@ -15,14 +18,14 @@ const BlogList = ({blogs}) => {
                         <p>{blog.city}</p>
                   
                     </Link>
+
                         
                 </div>
                  
                         
 
             ))}
-            
-    
+         
 
         </div>
     );
